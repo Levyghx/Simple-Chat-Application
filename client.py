ebab7e37ff48484a475663dedfc7a734414d6337
Levyghx <fulop.levente86@gmail.com>
2026-04-30 17:40:35 +0200

    Major changes.

--- client.py
+++ client.py
@@ -2,24 +2,71 @@ import socket
 import threading
 
 """
-STÁTUSZ KÓDOK:
-1 - ÜZENETFOGADÁS / MEGJELENÍTÉS
-2 - ÜZENETKÜLDÉS
+AZ ÜZENETEK FELÉPÍTÉSE:
+TÍPUSKÓD / KÜLDŐ NEVE / TARTALOM
 
-EGYÉB JELZÉSEK:
-/ - Elválasztó
+THE ARCHITECTURE OF MESSAGES:
+TYPE / SENDER USERNAME / CONTENT
+
+TÍPUSKÓDOK / TYPES:
+000 - ÉRVÉNYTELEN FELHASZNÁLÓNÉV INVALID USERNAME
+001 - ÉRVÉNYES FELHASZNÁLÓNÉV / VALID USERNAME
+0 - KLIENS KEZDEMÉNYEZÉS / INITIAL CLIENT REQUEST
+1 - ÜZENETFOGADÁS / RECEIVE MESSAGE
+2 - ÜZENETKÜLDÉS / SEND MESSAGE
+
+EGYÉB JELZÉSEK / OTHER SIGNALS:
+/ - ELVÁLASZTÓ // SEPARATOR
 """
+# Kezdeményezőfolyamat kezelő függvény / Initial connection handler
 
-# Üzenetmegjelenítés függvény
+def handleInitialConnection():
+    # csatlakozás a szerverhez, szál definiálása / connecting to the server, defining thread
+    try:
+        client.connect(("127.0.0.1", 9999))
+        print("\nKapcsolódás a szerverhez...\n")
+    except:
+        print("Nem sikerült kapcsolódni a szerverhez.\n")
+        quit()
+    else:
+        t_message_listener = threading.Thread(target=listenForMessage, args=())
+        t_message_listener.start()
+        starterFunction()
+
+# Kezdő függvény / Starter function
+
+def starterFunction():
+    # Név megadása / Name input
+    global name
+    name = input("\nKérem adjon meg egy felhasználónevet: ")
+    client.send(f"0/{name}/{name}".encode())
+
+# Üzenetmegjelenítés függvény / Message displaying
 
 def showMessage(data):
     usrname = data[1]
     message = data[2]
     print(f"{usrname}: {message}")
 
-# Új üzenet listener függvény
+# Üzenetkezelő függvény / Message handler
 
-def listenForMessage(client):
+def handleMessage(data):
+    data = data.split("/")
+    typeCode = data[0]
+
+    if typeCode == "1":
+        showMessage(data)
+    elif typeCode == "000":
+        print("\nÉrvénytelen név.")
+        starterFunction()
+    elif typeCode == "001":
+        print("Sikeres csatlakozás a Chat-hez.\n")
+        t_input_listener = threading.Thread(target=listenForInput, args=())
+        t_input_listener.start()
+
+# Új üzenet listener függvény / Incoming message listener
+
+def listenForMessage():
     while True:
         try:
             received_data = client.recv(1024).decode()
@@ -27,44 +74,21 @@ def listenForMessage(client):
             print("Megszakadt a kapcsolat.")
             break
         else:
-            received_data = received_data.split("/")
-            if received_data[0] == "1":
-                showMessage(received_data)
-            else:
-                pass
+            handleMessage(received_data)
 
-# Input listener függvény
+# Input listener függvény / Input listener
 
-def listenForInput(client):
+def listenForInput():
     while True:
         msg = input("")
         try:
             client.send(f"2/{name}/{msg}".encode())
         except:
-            print("Megszakadt a kapcsolat.")
+            print("Nem sikerült az üzenetet elküldeni.")
             break
 
-# Kliens socket létrehozása
-
+# Kliens socket létrehozása / Creating client socket
+global client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Név megadása
-name = input("\nKérem adjon meg egy felhasználónevet: ")
-print("\n Kapcsolódás a szerverhez...\n")
-
-# csatlakozás a szerverhez, szálak definiálása
-
-try:
-    client.connect(("127.0.0.1", 9999))
-    client.send(name.encode())
-except:
-    print("Nem sikerült kapcsolódni a szerverhez.\n")
-    quit()
-
-t_message_listener = threading.Thread(target=listenForMessage, args=(client,))
-t_input_listener = threading.Thread(target=listenForInput, args=(client,))
-
-print("Sikeres csatlakozás a Chat-hez.\n")
-
-t_message_listener.start()
-t_input_listener.start()
+handleInitialConnection()
--- server.py
+++ server.py
@@ -2,33 +2,37 @@ import socket
 import threading
 
 """
-STÁTUSZ KÓDOK:
-1 - ÜZENETFOGADÁS / MEGJELENÍTÉS
-2 - ÜZENETKÜLDÉS
+AZ ÜZENETEK FELÉPÍTÉSE:
+TÍPUSKÓD / KÜLDŐ NEVE / TARTALOM
 
-EGYÉB JELZÉSEK:
-/ - Elválasztó
+THE ARCHITECTURE OF MESSAGES:
+TYPE / SENDER USERNAME / CONTENT
+
+TÍPUSKÓDOK / TYPES:
+000 - ÉRVÉNYTELEN FELHASZNÁLÓNÉV INVALID USERNAME
+001 - ÉRVÉNYES FELHASZNÁLÓNÉV / VALID USERNAME
+0 - KLIENS KEZDEMÉNYEZÉS / INITIAL CLIENT REQUEST
+1 - ÜZENETFOGADÁS / RECEIVE MESSAGE
+2 - ÜZENETKÜLDÉS / SEND MESSAGE
+
+EGYÉB JELZÉSEK / OTHER SIGNALS:
+/ - ELVÁLASZTÓ // SEPARATOR
 """
 connected_clients = 0
 users = {}
 
-# Új kapcsolatok kezelése függvény
+# Új kapcsolatok kezelése függvény / Handling new connections
 
-def handleNewConnection(server):
+def handleNewConnection():
     while True:
-        # A kapcsolatok fogadása, nyilvántartása
+        # A kapcsolatok fogadása / Accepting connections
 
         client, addr = server.accept()
 
-        usrname = client.recv(1024).decode()
-
-        print(f"Új kliens csatlakozott: {usrname}, {client}")
-        users[usrname] = client
-        print(users)
-        t_input_listener = threading.Thread(target=listenForMessage, args=(client, usrname,))
+        t_input_listener = threading.Thread(target=listenForMessage, args=(client,))
         t_input_listener.start()
 
-# Küldő függvény (Replikáció)
+# Küldő függvény (Replikáció) / Replicator
 
 def replicateMessage(forwardable_data, senderUser):
     for user in users:
@@ -38,37 +42,50 @@ def replicateMessage(forwardable_data, senderUser):
             except:
                 print(f"Nem sikerült elküldeni az üzenetet {user} felhasználónak.")
 
-# Új üzenet listener függvény
+# Üzenetkezelő függvény / Message handler
 
-def listenForMessage(client, usrname):
+def handleMessage(data, client):
+    data = data.split("/")
+    typeCode = data[0]
+    username = data[1]
+    content = data[2]
+
+    if typeCode == "2":
+        data[0] = "1"
+        data = "/".join(data)
+        replicateMessage(data, username)
+    elif typeCode == "0":
+        if username in users:
+            client.send("000".encode())
+        else:
+            client.send("001".encode())
+            users[username] = client
+            print(f"Új user hozzáadva az adatlistához: {username}")
+
+# Új üzenet listener függvény / Incoming message listener
+
+def listenForMessage(client):
     while True:
         try:
             data = client.recv(1024).decode()
         except:
+            for k in users:
+                if users[k] == client:
+                    users.pop(k)
+                    break
             client.close()
-            try:
-                users.pop(usrname)
-            except:
-                print(f"{usrname} nincs az adatlistában!")
-            finally:
-                print(f"{usrname} lecsatlakozott!")
-                break
+            print("Egy kliens lecsatlakozott!")
+            break
         else:
-            data = data.split("/")
-            senderUser = data[1]
-            if data[0] == "2":
-                data[0] = "1"
-                forwardable_data = "/".join(data)
-                replicateMessage(forwardable_data, senderUser)
-            else:
-                pass
+            handleMessage(data, client)
                     
-# Szerver socket létrehozása, bindelés, és listenelés kapcsolatokért
+# Szerver socket létrehozása, bindelés, és listenelés kapcsolatokért / Creating server socket, binding, listening for connections
 
+global server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind(("127.0.0.1", 9999))
 server.listen()
 
-t_connection_listener = threading.Thread(target=handleNewConnection, args=(server,))
+t_connection_listener = threading.Thread(target=handleNewConnection, args=())
 t_connection_listener.start()
